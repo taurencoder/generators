c5d636406b124ef1be0de81e0c1d059221d8799e
Daniel Qin <danielqin@Daniel-ML00060061.local>
2014-10-23 16:11:26 +0800

    initalize demo project

---
+++ app.js
@@ -0,0 +1,20 @@
+var koa = require("koa");
+var router = require("koa-router");
+var parser = require("koa-bodyparser");
+var logger = require("koa-logger");
+var responseTime = require("koa-response-time");
+var app = koa();
+
+app.use(responseTime());
+app.use(logger());
+app.use(parser());
+
+app.use(function* (next){
+    this.body = "hello world!";
+    yield next;
+});
+
+app.listen("3000");
+console.log("listening to port 3000");
+
+module.exports = app;
